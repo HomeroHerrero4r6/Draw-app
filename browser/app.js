344347b14f0780c998118b24d4933f96887bd4ef
HomeroHerrero4r6 <61814990+HomeroHerrero4r6@users.noreply.github.com>
2020-12-03 21:44:37 -0300

    Drawing board
    made with vanilla javascript and socket connection

---
+++ browser/app.js
@@ -0,0 +1,33 @@
+  // var whiteboard = window.whiteboard;
+  // var socket = window.io(window.location.origin);
+  // var wboard = require('./whiteboard.js');
+
+  import {whiteboard} from './whiteboard';
+  // var io = require('socket.io-client');
+  // var socket = io(window.location.origin);
+  import io from 'socket.io-client';
+  var socket = io(window.location.origin);
+
+  socket.on('connect', function () {
+    console.log('Connected!');
+  });
+
+  socket.on('load', function (strokes) {
+
+    strokes.forEach(function (stroke) {
+      var start = stroke.start;
+      var end = stroke.end;
+      var color = stroke.color;
+      whiteboard.draw(start, end, color, false);
+    });
+
+  });
+
+  socket.on('draw', function (start, end, color) {
+    whiteboard.draw(start, end, color, false);
+  });
+
+  whiteboard.on('draw', function (start, end, color) {
+    socket.emit('draw', start, end, color);
+  });
+
